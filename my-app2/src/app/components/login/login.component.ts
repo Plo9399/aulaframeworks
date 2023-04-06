464f4555fb121280946e7d6b1b21ca167856cefb
Plo9399 <127432491+Plo9399@users.noreply.github.com>
2023-04-06 14:50:50 -0400

    Add files via upload

--- my-app2/src/app/components/login/login.component.ts
+++ my-app2/src/app/components/login/login.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isError = false;
+  isSuccess = false
 
-}
+  user = ""
+  pwd = ""
+
+  login() {
+
+    console.log("User= " + this.user + "Password = " + this.pwd)
+    if (this.user == "pedro.pessoal288" && this.pwd == "1234") {
+      this.isError = false;
+      this.isSuccess = true;
+      console.log("login Aprovado")
+    } else {
+      this.isError = true;
+      this.isSuccess = false;
+    }
+  }
+
+  }
