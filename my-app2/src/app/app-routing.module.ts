9a4f1248de7ac3e0e89389cbb4d0d2ff05e17f45
Plo9399 <127432491+Plo9399@users.noreply.github.com>
2023-04-10 16:46:42 -0400

    Add files via upload

--- my-app2/src/app/app-routing.module.ts
+++ my-app2/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HeroComponent } from './componets/hero/hero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'heroes', componet: HeroComponent},
+  {path: 'create', component: CreateUserComponent}, 
+  {path: 'login', componet: LoginComponent},
+  {path: '', redirecTo: '/login', pathMath: 'full'},
+  {path: '**', componet: PageNotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
